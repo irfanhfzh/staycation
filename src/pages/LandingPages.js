0555e66ead9a2247ec28750718c26e7063b3ef2e
irfanhfzh <irfanhafizh53@gmail.com>
2020-12-26 03:12:27 +0700

    Updates

---
+++ src/pages/LandingPages.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import moduleName from 'elements/Button'
+export default class LandingPages extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
