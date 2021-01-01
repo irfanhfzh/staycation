5d50754cc5985dc2921ac5c96a4e6de59a6236cd
irfanhfzh <irfanhafizh53@gmail.com>
2021-01-01 22:21:53 +0700

    Updates

--- src/parts/Categories.js
+++ src/parts/Categories.js
@@ -1,10 +1,12 @@
 import Button from 'elements/Button'
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 
 export default function Categories({ data }) {
     return data.map ( (category, index1) => {
         return (
         <section className="container" key={`category-${index1}`}>
+            <Fade bottom>
             <h4 className="mb-3 font-weight-medium">{category.name}</h4>
             <div className="container-grid">
                 {category.items.length === 0 ? ( 
@@ -20,6 +22,7 @@ export default function Categories({ data }) {
                                     className="item column-3 row-1" 
                                     key={`category-${index1}-item-${index2}`}
                                 >
+                                <Fade bottom delay={300 * index2}>
                                 <div className="card">
                                     {item.isPopular && (
                                     <div className="tag">
@@ -43,11 +46,13 @@ export default function Categories({ data }) {
                                         </span>
                                     </div>
                                 </div>
+                                </Fade>
                             </div>
                             ) 
                         })
                     )}
             </div>
+            </Fade>
         </section>
         )
     })
